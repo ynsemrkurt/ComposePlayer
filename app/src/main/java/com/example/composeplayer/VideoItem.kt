410bf450ccef4385fd82c5c1e8e1cd29817017a7
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-07-16 12:39:23 +0300

    Add VideoItem data class

---
+++ app/src/main/java/com/example/composeplayer/VideoItem.kt
@@ -0,0 +1,10 @@
+package com.example.composeplayer
+
+import android.net.Uri
+import androidx.media3.common.MediaItem
+
+data class VideoItem(
+    val contentUri: Uri,
+    val mediaItem: MediaItem,
+    val name: String,
+)
