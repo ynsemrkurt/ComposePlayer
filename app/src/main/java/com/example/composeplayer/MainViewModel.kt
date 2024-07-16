d055d9d060f6239031a7eb1577d6ecc1bf85b7f3
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-07-16 13:39:52 +0300

    Add video loading and playback functionality

--- app/src/main/java/com/example/composeplayer/MainViewModel.kt
+++ app/src/main/java/com/example/composeplayer/MainViewModel.kt
@@ -3,13 +3,50 @@ package com.example.composeplayer
 import android.net.Uri
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val state: SavedStateHandle
-
-): ViewModel() {
+    private val state: SavedStateHandle,
+    private val player: Player,
+    private val metaDataReader: MetaDataReader
+) : ViewModel() {
     private val videoUri = state.getStateFlow("videoUri", emptyList<Uri>())
+
+    private val videoItems = videoUri.map { uris ->
+        uris.map { uri ->
+            VideoItem(
+                contentUri = uri,
+                mediaItem = MediaItem.fromUri(uri),
+                name = metaDataReader.getMetaDataFromUri(uri)?.fileName ?: "Video"
+            )
+        }
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
+
+    init {
+        player.prepare()
+    }
+
+    fun addVideoUri(uri: Uri) {
+        state["videoUri"] = videoUri.value + uri
+        player.addMediaItem(MediaItem.fromUri(uri))
+    }
+
+    fun playVideo(uri: Uri) {
+        player.setMediaItem(
+            videoItems.value.find { it.contentUri == uri }?.mediaItem ?: return
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        player.release()
+    }
 }
