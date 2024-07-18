eac61bf3f2dc5a435d472c52296128c240c0959d
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-07-18 14:06:59 +0300

    feat: Display videos in a grid layout

--- app/src/main/java/com/plcoding/videoplayercompose/MainActivity.kt
+++ app/src/main/java/com/plcoding/videoplayercompose/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -15,9 +16,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
@@ -27,6 +28,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -34,6 +36,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.media3.ui.PlayerView
+import coil.compose.rememberImagePainter
 import com.plcoding.videoplayercompose.ui.theme.VideoPlayerComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -126,7 +129,10 @@ fun PlayerViewContainer(viewModel: MainViewModel, lifecycle: Lifecycle.Event) {
 
 @Composable
 fun VideoList(videoItems: List<VideoItem>, viewModel: MainViewModel) {
-    LazyColumn(modifier = Modifier.fillMaxWidth()) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 128.dp),
+        modifier = Modifier.fillMaxWidth()
+    ) {
         items(videoItems) { item ->
             VideoListItem(item = item, onVideoClick = { viewModel.playVideo(item.contentUri) })
         }
@@ -135,11 +141,21 @@ fun VideoList(videoItems: List<VideoItem>, viewModel: MainViewModel) {
 
 @Composable
 fun VideoListItem(item: VideoItem, onVideoClick: () -> Unit) {
-    Text(
-        text = item.name,
+    Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)
+            .padding(8.dp)
             .clickable(onClick = onVideoClick)
-    )
+    ) {
+        item.thumbnailUri?.let { thumbnailUri ->
+            Image(
+                painter = rememberImagePainter(data = thumbnailUri),
+                contentDescription = item.name,
+                modifier = Modifier
+                    .height(100.dp)
+                    .fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
 }
